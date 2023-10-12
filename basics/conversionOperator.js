d48519b2f7fa1b049adfe9fa187a6bdd69512525
anushka-82 <115973331+anushka-82@users.noreply.github.com>
2023-10-12 07:07:36 +0000

    container config done

---
+++ basics/conversionOperator.js
@@ -0,0 +1,14 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+// "33" => 33
+// "33abc"  NaN
+//  true = 1, false 0
+
---
+++ basics/dataType.js
@@ -0,0 +1,16 @@
+"use strict " // treat all js as newer version
+// alert(3+3) we are using nodde js, not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedin = false
+// number => 2 to pwr 53
+// bigint
+// tring => ""
+// boolean true/false
+// null standalone value
+// undefinded   value define/assign nhi kara
+// symbol => unique
+
+
+//object
